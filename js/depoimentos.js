80c628e3f530296822397fa68db5516835deca07
pawlaleme <165874717+pawlaleme@users.noreply.github.com>
2024-06-18 17:26:36 -0300

    Add files via upload

--- js/depoimentos.js
+++ js/depoimentos.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let depoimentos = document.querySelectorAll('.secao-depoimentos .depoimento');
     let index = 0;
 
     function showDepoimento(i, showAll = false) {
-        depoimentos.forEach(function(depoimento) {
+        depoimentos.forEach(function (depoimento) {
             depoimento.style.display = showAll ? 'block' : 'none';
         });
         if (!showAll) {
@@ -44,4 +44,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Add event listener for window resize
     window.addEventListener('resize', checkScreenSize);
+});
+
+//imagem saltando
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.secao-bonus img');
+
+    images.forEach(img => {
+        img.addEventListener('mouseenter', () => {
+            img.classList.add('hovered');
+        });
+
+        img.addEventListener('mouseleave', () => {
+        img.classList.remove('hovered');
+        });
+    });
 });
